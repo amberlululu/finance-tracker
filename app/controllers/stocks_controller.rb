6454728097e353d66dd28ec9787d7e7026a70978
Amber Bai <bai.lu3838@gmail.com>
2020-05-03 18:03:04 -0400

    Use Ajax to fix the invalid serch results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,25 @@
 class StocksController < ApplicationController
   def search
-     if params[:stock].present?
-        @stock = Stock.new_lookup(params[:stock])
-        if @stock
-          respond_to do |format|
-            format.js {render partial: 'users/result'}
-          end 
-        else
-          flash[:alert] = "Please enter a valid symbol to search" 
-          redirect_to my_portfolio_path
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js {render partial: 'users/result'}
         end 
-     else 
-        flash[:alert] = "Please enter a symbol to search"
-        redirect_to my_portfolio_path
-
-
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search" 
+          format.js{render partial: 'users/result'}
+        end  
+          
       end 
+    else 
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search" 
+        format.js{render partial: 'users/result'}
+      end  
+
     end 
+  end 
 
 end 
