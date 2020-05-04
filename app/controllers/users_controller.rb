99a94b55e99e2940b8daab17ca1254eeb55702a8
Amber Bai <bai.lu3838@gmail.com>
2020-05-03 20:19:49 -0400

    Create many to many association set up userstock resource and create stocks listing view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    # set a varable @tracked_stocks then in html you will be able to use this variable 
+    @tracked_stocks = current_user.stocks
   end
 end
