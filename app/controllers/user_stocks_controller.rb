094bc3cc1118f97ca9a5f1a175188e014478583b
Amber Bai <bai.lu3838@gmail.com>
2020-05-03 23:32:40 -0400

    Add functionality to remove tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,17 @@ class UserStocksController < ApplicationController
 
     end 
 
+
+    def destroy
+        # find the stock to be destroyed
+        stock = Stock.find(params[:id])
+        # find the right user corresponding to that stock
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        # delete the connection with user and stock
+        user_stock.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
+        redirect_to my_portfolio_path
+
+    end 
+
 end
